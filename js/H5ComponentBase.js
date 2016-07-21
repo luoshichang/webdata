521aec231961f54efd8e5f4d928f861ff182e42b
luoshichang <1245luo@gmail.com>
2016-07-21 14:43:19 +0800

    h5_component 组件

---
+++ js/H5ComponentBase.js
@@ -0,0 +1,31 @@
+/**
+ * Created by shichangluo on 2016/7/20.
+ */
+var H5ComponentBase = function(name,cfg){
+    var cfg = cfg || {};
+    var id = ('h5_c_'+Math.random()).replace('.','_')
+    var cls = 'h5_component_' + cfg.type+' h5_component_name'+name;
+    var component = $('<div class="h5_component '+cls+'" id="'+id+'">')
+    cfg.text && component.text(cfg.text)
+    cfg.width && component.width(cfg.width/2)
+    cfg.height && component.height(cfg.height/2) 
+    cfg.css && component.css(cfg.css)
+    cfg.bg && component.css('backgroundImage','url('+cfg.bg+')')
+    if( cfg.center === true){
+        component.css({
+            marginLeft : (cfg.width/4 * -1) + 'px',
+            left:'50%'
+        })
+    }
+    component.on('onLoad',function(){
+       component.addClass(cls+'_load').removeClass(cls+'_leave')
+        cfg.animateIn && component.animate(cfg.animateIn)
+        return false;
+    })
+    component.on('onLeave',function(){
+        component.addClass(cls+'_leave').removeClass(cls+'_load')
+        cfg.animateOut && component.animate(cfg.animateOut)
+        return false;
+    })
+    return component
+}
